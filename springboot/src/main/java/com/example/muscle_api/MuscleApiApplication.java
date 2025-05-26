83a7ab65b9723cacdce2f0f7d4a0a1084834b14c
sawada <appurupei64@gmail.com>
2025-05-26 21:35:09 +0900

    Initial commit for muscle training app

---
+++ springboot/src/main/java/com/example/muscle_api/MuscleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.muscle_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuscleApiApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/com/example/muscle_api/MuscleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muscle_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
